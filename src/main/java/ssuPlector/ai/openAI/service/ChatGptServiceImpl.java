5978b46860359fa3ecd8891e157d285068cbe065
Ares-10 <cholionking1@naver.com>
2024-08-06 22:55:57 +0900

    chore: 이미지 AI 및 브랜딩 AI 정확도 수정

--- src/main/java/ssuPlector/ai/openAI/service/ChatGptServiceImpl.java
+++ src/main/java/ssuPlector/ai/openAI/service/ChatGptServiceImpl.java
@@ -1,5 +1,8 @@
 package ssuPlector.ai.openAI.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -9,6 +12,7 @@ import ssuPlector.ai.openAI.dto.ChatGptRequest.ChatGptCommentRequest;
 import ssuPlector.ai.openAI.dto.ChatGptRequest.ChatGptImageRequest;
 import ssuPlector.ai.openAI.dto.ChatGptResponse.ChatGptCommentResponse;
 import ssuPlector.ai.openAI.dto.ChatGptResponse.ChatGptImageResponse;
+import ssuPlector.ai.openAI.dto.Message;
 import ssuPlector.global.exception.GlobalException;
 import ssuPlector.global.response.code.GlobalErrorCode;
 
@@ -33,7 +37,7 @@ public class ChatGptServiceImpl implements ChatGptService {
     @Override
     public String standardChat(String query, String model) {
         ChatGptCommentRequest request = new ChatGptCommentRequest(model, query);
-        ChatGptCommentResponse response = null;
+        ChatGptCommentResponse response;
         try {
             response = restTemplate.postForObject(apiUrl, request, ChatGptCommentResponse.class);
         } catch (Exception e) {
@@ -43,15 +47,65 @@ public class ChatGptServiceImpl implements ChatGptService {
     }
 
     @Override
-    public String makeImage(String query) {
-        ChatGptImageRequest request = new ChatGptImageRequest(query, 1, "256x256");
-        ChatGptImageResponse response = null;
+    public String makeImage(String imageInfo) {
+        List<Message> messages = new ArrayList<>();
+        messages.add(
+                new Message(
+                        "system",
+                        "We're going to use open ai's image generation API, "
+                                + "so we need to create a prompt in English to generate an image based on the text we enter. "
+                                + "Only give me the prompt."));
+        messages.add(new Message("user", imageInfo));
+
+        String imageGenerateQuery;
         try {
-            response = restTemplate.postForObject(imageUrl, request, ChatGptImageResponse.class);
+            ChatGptCommentRequest request = new ChatGptCommentRequest("gpt-4o", messages);
+            ChatGptCommentResponse response =
+                    restTemplate.postForObject(apiUrl, request, ChatGptCommentResponse.class);
+            imageGenerateQuery = response.getChoices().get(0).getMessage().getContent();
         } catch (Exception e) {
-            e.printStackTrace();
             throw new GlobalException(GlobalErrorCode._INTERNAL_SERVER_ERROR);
         }
-        return response.getData().get(0).getUrl();
+
+        System.out.println(imageGenerateQuery);
+
+        try {
+            ChatGptImageRequest request = new ChatGptImageRequest(imageGenerateQuery, 1, "512x512");
+            ChatGptImageResponse response =
+                    restTemplate.postForObject(imageUrl, request, ChatGptImageResponse.class);
+            return response.getData().get(0).getUrl();
+        } catch (Exception e) {
+            throw new GlobalException(GlobalErrorCode._INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Override
+    public String branding(String projectInfo) {
+        List<Message> messages = new ArrayList<>();
+        messages.add(
+                new Message(
+                        "system",
+                        "You are an assistant who creates (recommends) branding in a specific format. "
+                                + "You take input from ideas, topics, descriptions, etc. "
+                                + "The format of the branding you write looks like this: \n"
+                                + "<기본 요소>\n"
+                                + "1. 색상: [색상]\n"
+                                + "2. 폰트: [폰트]\n"
+                                + "3. 텍스처: [텍스처]\n"
+                                + "4. 슬로건: [슬로건]\n"
+                                + "<핵심 전략>\n"
+                                + "1. 대상 고객 이해하기: [맞춤 설명]\n"
+                                + "2. 앱 브랜딩을 위한 브랜드 아이덴티티 정의하기: [맞춤 설명]\n"
+                                + "3. 포괄적인 마케팅 계획 세우기: [맞춤 설명]\n"
+                                + "4. 사용자 경험에 집중하기: [맞춤 설명]"));
+        messages.add(new Message("user", projectInfo));
+        ChatGptCommentRequest request = new ChatGptCommentRequest("gpt-4o", messages);
+        ChatGptCommentResponse response;
+        try {
+            response = restTemplate.postForObject(apiUrl, request, ChatGptCommentResponse.class);
+        } catch (Exception e) {
+            throw new GlobalException(GlobalErrorCode._INTERNAL_SERVER_ERROR);
+        }
+        return response.getChoices().get(0).getMessage().getContent();
     }
 }
--- src/main/java/ssuPlector/service/designer/DesignerServiceImpl.java
+++ src/main/java/ssuPlector/service/designer/DesignerServiceImpl.java
@@ -13,24 +13,7 @@ public class DesignerServiceImpl implements DesignerService {
 
     @Override
     public String branding(String projectInfo) {
-        projectInfo =
-                "브랜드나 아이디어, 프로젝트의 주제 설명 등을 입력할게. 브랜딩을 형식에 맞춰서 구체적으로 작성(추천)해줘.\n"
-                        + "다음은 형식이야\n"
-                        + "<기본 요소>\n"
-                        + "1. 색상: [색상]\n"
-                        + "2. 폰트: [폰트]\n"
-                        + "3. 사운드: [사운드]\n"
-                        + "4. 텍스처: [텍스처]\n"
-                        + "5. 슬로건: [슬로건]\n"
-                        + "<핵심 전략>\n"
-                        + "1. 대상 고객 이해하기: [맞춤 설명]\n"
-                        + "2. 앱 브랜딩을 위한 브랜드 아이덴티티 정의하기: [맞춤 설명]\n"
-                        + "3. 브랜드 보이스 구축하기: [맞춤 설명]\n"
-                        + "4. 포괄적인 마케팅 계획 세우기: [맞춤 설명]\n"
-                        + "5. 사용자 경험에 집중하기: [맞춤 설명]\n"
-                        + "지금부터 브랜드나 아이디어, 프로젝트의 주제 설명 등을 입력할게.\n"
-                        + projectInfo;
-        return chatGptService.standardChat(projectInfo);
+        return chatGptService.branding(projectInfo);
     }
 
     @Override
